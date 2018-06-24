6a10f9d2da3b1ebcadaa3ffac82c660167a13a41
Jonathan Dahan <hi@jonathan.is>
2018-06-24 13:26:52 -0400

    initial commit

---
+++ main.rs
@@ -0,0 +1,36 @@
+#![feature(rust_2018_preview, use_extern_macros)]
+use clap;
+use std::fmt;
+
+pub struct Plugin {
+    pub author: String,
+    pub name: String,
+}
+
+impl fmt::Display for Plugin {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "{}/{}", self.author, self.name)?;
+        writeln!(formatter, "ok")?;
+        Ok(())
+    }
+}
+
+fn main() {
+    let plugin = Plugin {
+        author: "author".to_string(),
+        name: "name".to_string(),
+    };
+    let mut zr = clap_app!(zr =>
+        (version: crate_version!())
+        (author: "Jonathan Dahan <hi@jonathan.is>")
+        (about: "z:rat: - zsh plugin manager")
+        (@subcommand list => (about: "list plugins") )
+        (@subcommand load => (about: "load plugins fresh")
+            (@arg plugins: +required +multiple +takes_value "plugin/name[/path/to/file.zsh] [[plugin/name [..]..]")
+        )
+        (@subcommand update => (about: "update plugins") )
+    );
+    println!("{}", plugin);
+    println!("{:?}", zr)
+}
+
